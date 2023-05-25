99a7ac223faf93d5a2047d456ae64e923a1548e5
nesibecetin <nesibe3cetin@gmail.com>
2023-05-25 19:28:05 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { TodoComponent } from './component/todo/todo.component';
+import { CategoryComponent } from './component/category/category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:TodoComponent},
+  {path:'category',component:CategoryComponent},
+  {path:'todo',component:TodoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { TodoComponent } from './component/todo/todo.component';
+import { CategoryComponent } from './component/category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    TodoComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
